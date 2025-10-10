e1bbf5784224052a1416347d0f6dcb661804c00b
Saicharan Veldurthy <veldurthys1@gmail.com>
2025-10-10 09:15:27 -0700

    Fix base path for custom domain and add CNAME file

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/portfolio1/', // 👈 use your repo name here
+  base: '/',
   plugins: [react()],
 });
